69469c646f7586006d3e8fb1a6f0ac5a4977d080
liutuo <616669577@qq.com>
2022-04-14 20:10:06 +0800

    use online dataset

--- hw2/LinearNet.py
+++ hw2/LinearNet.py
@@ -2,23 +2,58 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
 import numpy as np
-from data_loader import load_data
 from torch.autograd import Variable
 
-X_train, Y_train, X_test, Y_test = load_data('../../Data/ml_data_hw2')
-X_train = torch.from_numpy(X_train).float()
-X_test = torch.from_numpy(X_test).float()
-Y_train = torch.from_numpy(np.array(Y_train)).long()
-Y_test = torch.from_numpy(np.array(Y_test)).long()
-
-train_num = X_train.shape[0]
-test_num = X_test.shape[0]
-
+# hyper parameter
+criterion = F.cross_entropy
 batch_size = 64
-
 num_inputs = 3072
 num_outputs = 10
+learning_rate = 1e-2
+momentum = 0.9
+num_epochs = 5
+
+# 数据增广方法
+transform = transforms.Compose([
+    # +4填充至36x36
+    transforms.Pad(4),
+    # 随机水平翻转
+    transforms.RandomHorizontalFlip(),
+    # 随机裁剪至32x32
+    transforms.RandomCrop(32),
+    # 转换至Tensor
+    transforms.ToTensor(),
+    #  归一化
+    #     transforms.Normalize(mean=(0.5, 0.5, 0.5),   # 3 for RGB channels
+    #                                      std=(0.5, 0.5, 0.5))
+])
+
+# cifar10路径
+cifar10Path = '../../Data/cifar'
+
+#  训练数据集
+train_dataset = torchvision.datasets.CIFAR10(root=cifar10Path,
+                                             train=True,
+                                             transform=transform,
+                                             download=True)
+
+# 测试数据集
+test_dataset = torchvision.datasets.CIFAR10(root=cifar10Path,
+                                            train=False,
+                                            transform=transform)
+
+# 生成数据加载器
+# 训练数据加载器
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+# 测试数据加载器
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
 
 
 class LinearNet(nn.Module):
@@ -32,63 +67,53 @@ class LinearNet(nn.Module):
         return F.softmax(self.linear(x), dim=1)
 
 
-net = LinearNet(num_inputs, num_outputs)
-optimizer = optim.SGD(params=net.parameters(), lr=0.1)
+model = LinearNet(num_inputs, num_outputs)
+optimizer = optim.SGD(params=model.parameters(),
+                      lr=learning_rate,
+                      momentum=momentum)
 
 
 def train(epoch):
     # 每次输入barch_idx个数据
-    batch_idx = 0
-    while (True):
-        down = batch_idx * batch_size
-        up = (batch_idx + 1) * batch_size if (
-            batch_idx + 1) * batch_size <= train_num else train_num
-        X = Variable(X_train[down:up, :])
-        Y = Variable(Y_train[down:up])
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = Variable(data), Variable(target)
 
         optimizer.zero_grad()
-        output = net(X)
+        output = model(data)
         # loss
-        loss = F.cross_entropy(output, Y)
-        loss = loss.requires_grad_()
+        loss = criterion(output, target)
         loss.backward()
         # update
         optimizer.step()
         if batch_idx % 100 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * batch_size, train_num,
-                100. * batch_idx * batch_size / train_num, loss.data.item()))
-            for parameters in net.parameters():
-                print(parameters)
-
-        if (up == train_num):
-            break
-
-        batch_idx += 1
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data.item()))
+            print(data.shape)
+            print(target.shape)
+            print(output.shape)
 
 
 def test():
-    test_loss = 0
-    correct = 0
-    # 测试集
-    for i in range(test_num):
-        X = X_test[i, :]
-        Y = Y_test[i].reshape(1)
+    with torch.no_grad():
+        test_loss = 0
+        correct = 0
+        # 测试集
+        for data, target in test_loader:
+            # data, target = Variable(data, volatile=True), Variable(target)
+            output = model(data)
+            # sum up batch loss
+            test_loss += criterion(output, target).data.item()
+            # get the index of the max
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
-        output = net(X)
-        # sum up batch loss
-        test_loss += F.cross_entropy(output, Y).data.item()
-        # get the index of the max
-        pred = output.data.max(1, keepdim=True)[1]
-        if (pred == Y):
-            correct += 1
-
-    test_loss /= test_num
-    print(
-        '\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-            test_loss, correct, test_num, 100. * correct / test_num))
+        test_loss /= len(test_loader.dataset)
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.
+              format(test_loss, correct, len(test_loader.dataset),
+                     100. * correct / len(test_loader.dataset)))
 
 
-for epoch in range(1, 100):
+for epoch in range(1, 10):
     train(epoch)
     test()
---
+++ hw2/bug.py
@@ -0,0 +1,95 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+from data_loader import load_data
+from torch.autograd import Variable
+
+X_train, Y_train, X_test, Y_test = load_data('../../Data/ml_data_hw2')
+X_train = torch.from_numpy(X_train).float().reshape(-1, 32, 32, 3)
+X_test = torch.from_numpy(X_test).float().reshape(-1, 32, 32, 3)
+Y_train = torch.from_numpy(np.array(Y_train)).long()
+Y_test = torch.from_numpy(np.array(Y_test)).long()
+
+train_num = X_train.shape[0]
+test_num = X_test.shape[0]
+
+batch_size = 64
+
+num_inputs = 3072
+num_outputs = 10
+
+
+class LinearNet(nn.Module):
+
+    def __init__(self, num_inputs, num_outputs):
+        super(LinearNet, self).__init__()
+        self.linear = nn.Linear(num_inputs, num_outputs)
+
+    def forward(self, x):  # x shape: (batch, 1, 28, 28)
+        x = x.view(-1, num_inputs)
+        return F.softmax(self.linear(x), dim=1)
+
+
+net = LinearNet(num_inputs, num_outputs)
+optimizer = optim.SGD(params=net.parameters(), lr=0.1)
+
+
+def train(epoch):
+    # 每次输入barch_idx个数据
+    batch_idx = 0
+    while (True):
+        down = batch_idx * batch_size
+        up = (batch_idx + 1) * batch_size if (
+            batch_idx + 1) * batch_size <= train_num else train_num
+        data = Variable(X_train[down:up, :])
+        target = Variable(Y_train[down:up])
+
+        optimizer.zero_grad()
+        output = net(data)
+        # loss
+        loss = F.cross_entropy(output, target)
+        loss = loss.requires_grad_()
+        loss.backward()
+        # update
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * batch_size, train_num,
+                100. * batch_idx * batch_size / train_num, loss.data.item()))
+            '''print(data.shape)
+            print(target.shape)
+            print(output.shape)'''
+
+        if (up == train_num):
+            break
+
+        batch_idx += 1
+
+
+def test():
+    test_loss = 0
+    correct = 0
+    # 测试集
+    for i in range(test_num):
+        X = X_test[i, :]
+        Y = Y_test[i].reshape(1)
+
+        output = net(X)
+        # sum up batch loss
+        test_loss += F.cross_entropy(output, Y).data.item()
+        # get the index of the max
+        pred = output.data.max(1, keepdim=True)[1]
+        if (pred == Y):
+            correct += 1
+
+    test_loss /= test_num
+    print(
+        '\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, test_num, 100. * correct / test_num))
+
+
+for epoch in range(1, 100):
+    train(epoch)
+    test()
