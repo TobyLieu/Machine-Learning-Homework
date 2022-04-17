cf64c364a57f0a35a9d40709a9918ecea6c1cf8a
liutuo <616669577@qq.com>
2022-04-17 16:14:30 +0800

    finished

--- hw2/main.py
+++ hw2/main.py
@@ -10,30 +10,31 @@ import matplotlib.pyplot as plt
 
 # hyper parameter
 criterion = nn.CrossEntropyLoss()
-batch_size = 64
+batch_size = 128
 num_inputs = 3072
 num_outputs = 10
 learning_rate = 1e-2
-momentum = 0.5
+momentum = 0.8
+beta1 = 0.99
 num_epochs = 50
 
 hidden_layer_size = 5
 layer_num = 2
 kenel_num = 6
-net_type = 'cnn'
+net_type = 'resnet'
 
 # 数据预处理
-transform = transforms.Compose([
-    # +4填充至36x36
-    transforms.Pad(4),
-    # 随机水平翻转
-    transforms.RandomHorizontalFlip(),
-    # 随机裁剪至32x32
-    transforms.RandomCrop(32),
-    # 转换至Tensor
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),  #先四周填充0，在吧图像随机裁剪成32*32
+    transforms.RandomHorizontalFlip(),  #图像一半的概率翻转，一半的概率不翻转
     transforms.ToTensor(),
-    # 归一化
-    transforms.Normalize(std=(0.5, 0.5, 0.5), mean=(0.5, 0.5, 0.5))
+    transforms.Normalize((0.4914, 0.4822, 0.4465),
+                         (0.2023, 0.1994, 0.2010)),  #R,G,B每层的归一化用到的均值和方差
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 # cifar10路径
@@ -42,12 +43,12 @@ cifar10Path = '~/Data/cifar'
 #  训练数据集
 train_dataset = torchvision.datasets.CIFAR10(root=cifar10Path,
                                              train=True,
-                                             transform=transform,
+                                             transform=transform_train,
                                              download=True)
 # 测试数据集
 test_dataset = torchvision.datasets.CIFAR10(root=cifar10Path,
                                             train=False,
-                                            transform=transform)
+                                            transform=transform_test)
 
 # 训练数据加载器
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
@@ -105,12 +106,90 @@ class CNNNet(nn.Module):
         return x
 
 
-# model = LinearNet(num_inputs, num_outputs)
-# model = MLPNet(num_inputs, num_outputs)
-model = CNNNet()
+class ResidualBlock(nn.Module):
+    def __init__(self, inchannel, outchannel, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.left = nn.Sequential(
+            nn.Conv2d(inchannel,
+                      outchannel,
+                      kernel_size=3,
+                      stride=stride,
+                      padding=1,
+                      bias=False), nn.BatchNorm2d(outchannel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(outchannel,
+                      outchannel,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1,
+                      bias=False), nn.BatchNorm2d(outchannel))
+        self.shortcut = nn.Sequential()
+        if stride != 1 or inchannel != outchannel:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(inchannel,
+                          outchannel,
+                          kernel_size=1,
+                          stride=stride,
+                          bias=False), nn.BatchNorm2d(outchannel))
+
+    def forward(self, x):
+        out = self.left(x)
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, ResidualBlock, num_classes=10):
+        super(ResNet, self).__init__()
+        self.inchannel = 64
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+        self.layer1 = self.make_layer(ResidualBlock, 64, 2, stride=1)
+        self.layer2 = self.make_layer(ResidualBlock, 128, 2, stride=2)
+        self.layer3 = self.make_layer(ResidualBlock, 256, 2, stride=2)
+        self.layer4 = self.make_layer(ResidualBlock, 512, 2, stride=2)
+        self.fc = nn.Linear(512, num_classes)
+
+    def make_layer(self, block, channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)  #strides=[1,1]
+        layers = []
+        for stride in strides:
+            layers.append(block(self.inchannel, channels, stride))
+            self.inchannel = channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+
+def ResNet18():
+
+    return ResNet(ResidualBlock)
+
+
+# model = LinearNet()
+# model = MLPNet()
+# model = CNNNet()
+model = ResNet18()
 optimizer = optim.SGD(params=model.parameters(),
                       lr=learning_rate,
                       momentum=momentum)
+'''optimizer = optim.Adam(params=model.parameters(),
+                       lr=learning_rate,
+                       betas=(beta1, 0.999))'''
+# optimizer = optim.SGD(params=model.parameters(), lr=learning_rate)
 
 if (torch.cuda.is_available()):
     model = model.cuda()
@@ -168,9 +247,11 @@ def test(epoch):
 
 if __name__ == '__main__':
     for epoch in range(1, num_epochs):
+        if (epoch == 20):
+            learning_rate = 1e-3
         train(epoch)
         test(epoch)
 
     x = np.array(x)
     acc_list = np.array(acc_list)
-    np.savez('./result/' + net_type + '/momentum=' + momentum, x, acc_list)
+    np.savez('./result/' + net_type + '/lr_alter, batch_size=128', x, acc_list)
